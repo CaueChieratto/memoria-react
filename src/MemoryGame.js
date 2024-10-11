5f131ff258fedb9ec641da80b8ea0a703bcf899f
CaueChieratto <caue.chieratto@icloud.com>
2024-10-11 10:41:19 -0300

    mobile finished

--- src/MemoryGame.js
+++ src/MemoryGame.js
@@ -1,36 +1,39 @@
 import React, { useEffect, useState } from "react";
 import GameOver from "./components/GameOver";
-import game from "./game/game"
+import game from "./game/game";
 import GameBoard from "./components/GameBoard";
 
 export default function MemoryGame() {
+  const [gameOver, setGameOver] = useState(true);
+  const [cards, setCards] = useState([]);
 
-    const [gameOver, setGameOver] = useState(false);
-    const [cards, setCards] = useState([])
+  useEffect(() => {
+    setCards(game.createCardsFromTechs());
+  }, []);
 
-    useEffect(() => {
-        setCards(game.createCardsFromTechs())
-    }, [])
+  function restart() {
+    game.clearCards();
+    setCards(game.createCardsFromTechs());
+    setGameOver(false);
+  }
 
-    function restart() {
-        game.clearCards()
-        setCards(game.createCardsFromTechs())
-        setGameOver(false)
-    }
+  function handleFlip(card) {
+    game.flipCard(
+      card.id,
+      () => {
+        setGameOver(true);
+      },
+      () => {
+        setCards([...game.cards]);
+      }
+    );
+    setCards([...game.cards]);
+  }
 
-    function handleFlip(card) {
-        game.flipCard(card.id, ()=>{
-            setGameOver(true)
-        },()=>{
-            setCards([...game.cards])
-        })
-        setCards([...game.cards])
-    }
-
-    return (
-        <div>
-            <GameBoard handleFlip = {handleFlip} cards = {cards}></GameBoard>
-            <GameOver show = {gameOver} handleRestart={restart}></GameOver>
-        </div>
-    )
+  return (
+    <div>
+      <GameBoard handleFlip={handleFlip} cards={cards}></GameBoard>
+      <GameOver show={gameOver} handleRestart={restart}></GameOver>
+    </div>
+  );
 }
